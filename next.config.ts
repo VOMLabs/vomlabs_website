cbe10a1535cda812ae605084645a13262397156e
ItzzMateo <itzzmateo@devflare.de>
2026-06-13 01:03:20 +0200

    feat(docker): setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
@@ -17,7 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  /* config options here */
 };
 
 export default nextConfig;
